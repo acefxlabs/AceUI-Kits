badbb247ab815de954b914ee5db40d0d540e3754
Oyewumi Abayomi <ace@acefxlab.com>
2015-07-21 00:11:06 +0100

    Dynamic Form
    Any dynamic form can now respond.
    Changed Submit Action (line 12) to on Submit (line 35)

--- bx-invalidate.js
+++ bx-invalidate.js
@@ -3,14 +3,19 @@
  * https://boxedall.com
  * This script is written for all lovers of the web who belives things should be done easily without stress
  */
- 
-$('.validateMe:visible').submit(function(evt){
+
+$(document).ready(function(){
+    $('body').prepend('<style>.bx-error{ float: left; width: 100%; font-size: 70%; color: #FF0000; margin-top: 0px; padding-bottom: 10px; }</style>');
+});
+
+
+/* $('.validateMe:visible').submit(function(evt){
     evt.preventDefault();
     var formname = $(this).attr('name');
     validateMe(formname);
-});
+});*/
 
-$('.validateMe:visible').on('keydown', '.validate', function(e){
+$('body').on('keydown', '.validateMe:visible .number', function(e){
     if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
              // Allow: Ctrl+A
             (e.keyCode == 65 && e.ctrlKey === true) ||
@@ -27,53 +32,63 @@ $('.validateMe:visible').on('keydown', '.validate', function(e){
         if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
             e.preventDefault();
         }
+}).on('submit', '.validateMe:visible', function(evt){
+    evt.preventDefault();
+    var formname = $(this).attr('name');
+    validateMe(formname);
 });
  
 function validateMe(formname){ 
-    
     //Gneeral Validation to ensure it is filled
     $('form[name='+formname+']:visible .required').each(function(){
-        $(this).next('.error').remove();
+        $(this).next('.bx-error').remove();
         //var toPranet = $(this).parent('.item');
         if($(this).val() == '' || $(this).val() == null){
-            $(this).after('<div class="smfont red-text tright left full error">This field must not be empty</div>');
+            $(this).after('<div class="smfont red-text tright left full bx-error">This field must not be empty</div>');
         }else{
-            $(this).next('.error').remove();
+            $(this).next('.bx-error').remove();
         }
     });
     
     //Email
     $('form[name='+formname+']:visible .email').each(function(){
         
-        $(this).next('.error').remove();
+        $(this).next('.bx-error').remove();
         
         var email = $(this).val();
         var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
         
         if(re.test(email) == false){
-            $(this).after('<div class="smfont red-text tright left full error">Invalid email format</div>');
+            $(this).after('<div class="smfont red-text tright left full bx-error">Invalid email format</div>');
         }else{
-            $(this).next('.error').remove(); 
+            $(this).next('.bx-error').remove(); 
         }
     });
     
     //Number
     $('form[name='+formname+']:visible .number').each(function(){
-        $(this).next('.error').remove();
+        $(this).next('.bx-error').remove();
         var num = $(this).val();
         if(isNaN(num) || num == ''){
-           $(this).after('<div class="smfont red-text tright left full error">Invalid number format</div>');
+           $(this).after('<div class="smfont red-text tright left full bx-error">Invalid number format</div>');
         }else{
-            $(this).next('.error').remove();
+            $(this).next('.bx-error').remove();
         }
     });
     
     
     
     //Check loop
-    if($('form[name='+formname+']:visible .error').length > 0){
+    if($('form[name='+formname+']:visible .bx-error').length > 0){
         return false; //Stop form completely
     }else{
-        return true; //
+        var call = $('form[name='+formname+']:visible').attr('callback');
+        
+        //alert($(this).attr('callback'));
+        if(call != null && call != ''){
+            window[call](formname);
+        }else{
+            $('form[name='+formname+']:visible').submit();
+        }
     }
 }
